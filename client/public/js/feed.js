bd3cbc078e2d58d4aea4de7147c6e8c277824860
Arnold Sosa <as3162@students.necsd.net>
2024-07-11 15:38:56 -0400

    Modified feed.js

--- client/public/js/feed.js
+++ client/public/js/feed.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function removeNewsfeed() {
         let newsfeedElement = document.getElementById('newsfeed');
         if (newsfeedElement) {
-            newsfeedElement.innerHTML = '';
+            newsfeedElement.innerHTML = 'Newsfeed stories are below';
         }
     }
 
